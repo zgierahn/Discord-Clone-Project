6a43ae60aa4586678eed655aba6d7f1966bb8c76
MattBoyer <matthewryanboyer@yahoo.com>
2023-07-28 14:31:38 -0700

    fixed

--- app/models/serveruser.py
+++ app/models/serveruser.py
@@ -1,5 +1,5 @@
 from .db import db
-
+from .db import environment, SCHEMA
 
 server_user = db.Table(
     'server_user',
@@ -7,3 +7,5 @@ server_user = db.Table(
     db.Column("servers", db.Integer, db.ForeignKey("servers.id"), primary_key=True),
     db.Column("users", db.Integer, db.ForeignKey("users.id"), primary_key=True)
 )
+if environment == "production":
+    server_user.schema = SCHEMA
