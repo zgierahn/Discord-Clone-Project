2daf7a82c6f456131c36beea6582e67ac2ba966d
MattBoyer <matthewryanboyer@yahoo.com>
2023-07-28 20:34:30 -0700

    single server

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -17,10 +17,17 @@ session = SessionFactory()
 @server_routes.route('/<int:id>/<int:serverId>')
 @login_required
 def single_server(id,serverId):
-    servers = session.query.join(User).join(Channel).join(Reaction).filter(User.id == id, Server.id == serverId) #Server.id == serverId
+    servers = db.session.query(User,Channel,Server,).join(User,Server).filter(User.id ==id, Channel.server_id == serverId)
     # servers = session.query(User).join(Server).filter(User.id == id)
-    print('-----------------this is my servers------------------', servers.values())
+    # res = []
+    # for server in servers:
+    #     answer = []
+    #     for ele in server:
+    #         answer.append(ele.to_dict())
+    #     res.append(answer)
+    print([server.to_dict() for server in servers])
     """
+    print('-----------------this is my servers------------------', servers)
     servers = Server.query.join(User, Server.user).filter(User.id ==id, Server.id == serverId) #Server.id == serverId
     Query for all users and returns them in a list of user dictionaries
     return {'servers': [server.to_dict() for server in servers]}
