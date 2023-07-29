660e0f2e4c85815c5800a4e7bd35e50d2b6579c9
MattBoyer <matthewryanboyer@yahoo.com>
2023-07-28 17:14:42 -0700

    wiorking on single server

---
+++ react-app/src/components/SingleServer/index.js
@@ -0,0 +1,25 @@
+import { thunkGetSingleServer } from "../../store/servers"
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+import { useParams } from 'react-router-dom'
+
+function SingleServer() {
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const { userId,serverId } = useParams()
+
+    useEffect(() => {
+        dispatch(thunkGetSingleServer(userId))
+    }, [dispatch])
+
+    const server = useSelector(state => Object.values(state.servers.singleServer))
+    return (
+        <div>
+            TEST
+        </div>
+    )
+}
+
+export default SingleServer
