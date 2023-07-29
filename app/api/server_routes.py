23b2a3bfefefb316d6f863c77150b08a1ccb2f24
Zach Gierahn <zkg2789@yahoo.com>
2023-07-28 18:55:14 -0700

    testing-sql-route

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -1,21 +1,31 @@
-from flask import Blueprint, jsonify, session
+from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import Server, User
+from app.models import Server, User, Channel, Message, Reaction
 from app.models import db
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 server_routes = Blueprint('servers', __name__)
+
+#####test stuff#######
+db_url = "sqlite:///dev.db"
+engine = create_engine(db_url)
+SessionFactory = sessionmaker(bind=engine)
+session = SessionFactory()
+############################
 
 @server_routes.route('/<int:id>/<int:serverId>')
 @login_required
 def single_server(id,serverId):
-    servers = Server.query.join(User, Server.user).filter(User.id ==id,Server.id == serverId)
+    servers = session.query.join(User).join(Channel).join(Reaction).filter(User.id == id, Server.id == serverId) #Server.id == serverId
+    # servers = session.query(User).join(Server).filter(User.id == id)
+    print('-----------------this is my servers------------------', servers.values())
     """
-    servers = servers[0].to_dict()
-    servers = Server.query.all()
+    servers = Server.query.join(User, Server.user).filter(User.id ==id, Server.id == serverId) #Server.id == serverId
     Query for all users and returns them in a list of user dictionaries
     return {'servers': [server.to_dict() for server in servers]}
     """
-    return [server.to_dict() for server in servers]
+    return {"key": "test"}
 
 @server_routes.route('/<int:id>')
 @login_required
@@ -28,3 +38,10 @@ def servers(id):
     return {'servers': [server.to_dict() for server in servers]}
     """
     return [server.to_dict() for server in servers]
+
+
+
+
+###########test stuff#############
+session.close()
+engine.dispose()
