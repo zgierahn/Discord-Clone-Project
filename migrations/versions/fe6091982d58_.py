99551ed28f8f075abf41939657cc4d6f84bf80f7
Michael <michael.oyola@outlook.com>
2023-08-06 16:41:52 -0400

    committed

--- migrations/versions/fe6091982d58_.py
+++ migrations/versions/fe6091982d58_.py
@@ -2,11 +2,11 @@
 
 <<<<<<<< HEAD:migrations/versions/fe6091982d58_.py
 Revision ID: fe6091982d58
-Revises: 
+Revises:
 Create Date: 2023-08-05 13:39:16.088226
 ========
 Revision ID: 4c89e463e885
-Revises: 
+Revises:
 Create Date: 2023-08-05 11:28:51.844449
 >>>>>>>> 8baa843 (servers):migrations/versions/4c89e463e885_.py
 
@@ -16,11 +16,9 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-<<<<<<<< HEAD:migrations/versions/fe6091982d58_.py
+
 revision = 'fe6091982d58'
-========
-revision = '4c89e463e885'
->>>>>>>> 8baa843 (servers):migrations/versions/4c89e463e885_.py
+
 down_revision = None
 branch_labels = None
 depends_on = None
