b051fd74624595aa800e28c25c47f0d633137587
Michael <Michael_oyola@outlook.com>
2023-09-09 14:40:43 -0400

    changed backend configure url so the chat socket goes to the right url line 9 and 10 in socketio_test.py

--- app/socketio_test.py
+++ app/socketio_test.py
@@ -6,8 +6,8 @@ from .models import Message, db
 # configure cors_allowed_origins
 if os.environ.get('FLASK_ENV') == 'production':
     origins = [
-        'http://discord-flask-clone.onrender.com',
-        'https://discord-flask-clone.onrender.com'
+        'http://flaskcord-clone.onrender.com/',
+        'https://flaskcord-clone.onrender.com/'
     ]
 else:
     origins = "*"
